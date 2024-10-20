09f5cbb5e41d40931e0a4afa46ec7009de210cff
Mahesh <maheshpansare099+Github@gmail.com>
2024-10-20 17:50:25 +0530

    base:

--- server.tf
+++ server.tf
@@ -10,9 +10,8 @@ data "aws_ami" "latest-amazon-linux-image" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-}# Define the Jenkins server
+}
 resource "aws_instance" "jenkins-server" {
-  # Specify the AMI id defined above
   ami                         = data.aws_ami.latest-amazon-linux-image.id
   instance_type               = var.instance_type
   key_name                    = "jenkins-server-demo"
@@ -20,7 +19,6 @@ resource "aws_instance" "jenkins-server" {
   vpc_security_group_ids      = [aws_default_security_group.jenkins-sg.id]
   availability_zone           = var.availability_zone
   associate_public_ip_address = true
-  # Specify a script to be executed when the instance is launched
   user_data                   = file("jenkins-script.sh")
   tags = {
     Name = "${var.env_prefix}-server"
