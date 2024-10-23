9e3a13d9b7558db950c20603cbc5c86cafe0765b
Mahesh Pansare <138249606+epic-croswords@users.noreply.github.com>
2024-10-23 22:12:47 +0530

    Update eks.tf

--- eks-cluster/eks.tf
+++ eks-cluster/eks.tf
@@ -5,14 +5,16 @@ module "eks" {
     cluster_version = "1.24"    
     cluster_endpoint_public_access  = true    
     vpc_id = module.my-vpc.vpc_id
-    subnet_ids = module.my-vpc.private_subnets    tags = {
+    subnet_ids = module.my-vpc.private_subnets    
+    tags = {
         environment = "development"
         application = "myapp"
     }    eks_managed_node_groups = {
         dev = {
             min_size = 1
             max_size = 3
-            desired_size = 2            instance_types = ["t2.small"]
+            desired_size = 2            
+            instance_types = ["t2.small"]
         }
     }
 }
