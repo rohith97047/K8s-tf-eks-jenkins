7544bbe5b53ce916ca859b99285c50dd20dd378e
Mahesh Pansare <138249606+epic-croswords@users.noreply.github.com>
2024-10-23 22:13:14 +0530

    Update vpc.tf

--- eks-cluster/vpc.tf
+++ eks-cluster/vpc.tf
@@ -6,14 +6,17 @@ module "my-vpc" {
   cidr            = var.vpc_cidr_block
   private_subnets = var.private_subnet_cidr_blocks
   public_subnets  = var.public_subnet_cidr_blocks
-  azs             = data.aws_availability_zones.azs.names  enable_nat_gateway   = true
+  azs             = data.aws_availability_zones.azs.names  
+  enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true  tags = {
     "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
-  }  public_subnet_tags = {
+  }  
+  public_subnet_tags = {
     "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
     "kubernetes.io/role/elb"                  = 1
-  }  private_subnet_tags = {
+  }  
+  private_subnet_tags = {
     "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
     "kubernetes.io/role/internal-elb"         = 1
   }
